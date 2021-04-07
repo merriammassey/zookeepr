10a5a76bdad83d012a1656851cb47519f6b8ddd6
Maria M <prov21_1@hotmail.com>
2021-04-06 20:18:10 -0700

    more errors

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function filterByQuery(query, animalsArray) {
 }
 
 //access query property on req object
-app.get('/api/animals', (req, res) => {
+app.get('./api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
